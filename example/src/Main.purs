cba20cbba733a3f427ef62b731817bba5114af22
TANABE Takashi <narinari.t@gmail.com>
2017-03-21 00:03:09 +0900

    Bump up Halogen.

--- example/src/Main.purs
+++ example/src/Main.purs
@@ -1,22 +1,20 @@
 module Main where
 
-import Prelude (class Eq, class Ord, type (~>), Unit, pure, bind, (=<<), ($), (==))
-import Control.Monad.Eff (Eff)
-import Control.Monad.Aff (Aff)
-
-import Data.Functor.Coproduct (Coproduct())
-import Data.Maybe (Maybe(..))
-
-import Halogen as H
-import Halogen.HTML.Indexed as HH
-import Halogen.HTML.Events.Indexed as HE
-import Halogen.HTML.Properties.Indexed as HP
-import Halogen.Util (runHalogenAff, awaitBody)
-
 import QRCode.Types
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
 import QRCode.Halogen.Component as QRCode
+import Control.Monad.Aff (Aff)
+import Control.Monad.Eff (Eff)
+import Data.Maybe (Maybe(..))
+import Data.Void (Void, absurd)
+import Halogen.Aff (HalogenEffects, runHalogenAff, awaitBody)
+import Halogen.VDom.Driver (runUI)
+import Prelude (class Eq, class Ord, type (~>), Unit, unit, const, (=<<), ($), (==), ($>), (*>))
 
-type MainEffects = H.HalogenEffects (qrcode :: QRCODE)
+type MainEffects = HalogenEffects (qrcode :: QRCODE)
 
 data Query a
   = UpdateText String String a
@@ -37,55 +35,45 @@ data QRCodeSlot = QRCodeSlot String
 derive instance qrcodeSlotOrd :: Ord QRCodeSlot
 derive instance qrcodeSlotEq :: Eq QRCodeSlot
 
-type StateP g = H.ParentState State QRCode.State Query QRCode.Query g QRCodeSlot
-type QueryP = Coproduct Query (H.ChildF QRCodeSlot QRCode.Query)
-type MainHTML g = H.ParentHTML QRCode.State Query QRCode.Query g QRCodeSlot
+type MainHTML g = H.ParentHTML Query QRCode.Query QRCodeSlot g
 type MainAff = Aff MainEffects
-type MainDSL g = H.ParentDSL State QRCode.State Query QRCode.Query g QRCodeSlot
+type MainDSL g = H.ParentDSL State Query QRCode.Query QRCodeSlot Void g
 
 
-ui :: H.Component (StateP MainAff) QueryP MainAff
-ui = H.parentComponent { render, eval, peek: Nothing }
+ui :: H.Component HH.HTML Query Unit Void MainAff
+ui = H.parentComponent { initialState: const initialState, render, eval, receiver: const Nothing }
   where
   qrcodeConfig = defaultConfig { height = 80, width = 80 }
   render :: State -> MainHTML MainAff
   render state =
     HH.div_
       [ HH.div_
-        [ HH.slot (QRCodeSlot "A")
-            \_ -> { component: QRCode.component
-                  , initialState: QRCode.initialState
-                      { config = qrcodeConfig { text = state.textA } }
-                  }
+        [ HH.slot (QRCodeSlot "A") (QRCode.component Nothing) Nothing absurd
         , HH.text "QR Code"
         , HH.input
-            [ HP.inputType HP.InputText
+            [ HP.type_ HP.InputText
             , HP.placeholder "URL"
             , HP.value state.textA
             , HE.onValueChange $ HE.input $ UpdateText "A"
             ]
         ]
       , HH.div_
-        [ HH.slot (QRCodeSlot "B")
-            \_ -> { component: QRCode.component
-                  , initialState: QRCode.initialState
-                      { config = qrcodeConfig { text = state.textB } }
-                  }
+        [ HH.slot (QRCodeSlot "B") (QRCode.component $ Just qrcodeConfig) (Just state.textB) absurd
         , HH.text "QR Code"
         , HH.input
-            [ HP.inputType HP.InputText
+            [ HP.type_ HP.InputText
             , HP.placeholder "URL"
             , HP.value state.textB
             , HE.onValueChange $ HE.input $ UpdateText "B"
             ]
         ]
       ]
-  
+
   eval :: Query ~> MainDSL MainAff
-  eval (UpdateText slotID newText next) = do
+  eval (UpdateText slotID newText next) =
     H.modify (\s -> if slotID == "A" then s { textA = newText } else s { textB = newText })
-    H.query (QRCodeSlot slotID) $ H.action $ QRCode.SetText newText
-    pure next
- 
+    *> H.query (QRCodeSlot slotID) (H.action $ QRCode.SetText $ Just newText)
+    $> next
+
 main :: Eff MainEffects Unit
-main = runHalogenAff $ H.runUI ui (H.parentState initialState) =<< awaitBody
+main = runHalogenAff $ runUI ui unit =<< awaitBody
--- src/QRCode/Halogen/Component.purs
+++ src/QRCode/Halogen/Component.purs
@@ -1,18 +1,16 @@
 module QRCode.Halogen.Component where
 
-import Prelude (type (~>), pure, void, unit, bind, const, (<$>), (<<<), ($), ($>))
-
-import QRCode (Config, QRCode, QRCODE, makeCode, mkQRCodeNode, defaultConfig)
-
-import Control.Monad.Aff.Class (class MonadAff)
-import Control.Monad.Aff.Free (class Affable)
-import Data.Maybe (Maybe(..), maybe)
-
-import DOM.HTML.Types (HTMLElement)
-
 import Halogen as H
-import Halogen.HTML.Indexed as HH
-import Halogen.HTML.Properties.Indexed as HP
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
+import Control.Monad.Aff.Class (class MonadAff)
+import DOM.HTML.Types (HTMLElement)
+import Data.Maybe (Maybe(..), maybe, fromMaybe)
+import Data.Traversable (traverse_)
+import Data.Void (Void)
+import Prelude (type (~>), const, pure, bind, unit, (<*>), (<$>), ($))
+import QRCode (Config, QRCode, QRCODE, makeCode, mkQRCodeNode, defaultConfig)
 
 type QREffects eff = (qrcode :: QRCODE | eff)
 
@@ -23,47 +21,50 @@ type State =
   }
 
 data Query a
-  = SetElement (Maybe HTMLElement) a
-  | Init a
-  | SetText String a
+  = Init a
+  | SetText Input a
 
-initialState :: State
-initialState = 
+type Input = Maybe String
+
+initialState :: Config -> Input -> State
+initialState config text =
   { element: Nothing
   , qrcode: Nothing
-  , config: defaultConfig
+  , config: maybe config (\t -> config { text = t }) text
   }
 
 component
   :: forall m eff
   . ( MonadAff (QREffects eff) m
-    , Affable (QREffects eff) m
     )
-  => H.Component State Query m
-component = H.lifecycleComponent
-  { render
+  => Maybe Config -> H.Component HH.HTML Query Input Void m
+component cfg = H.lifecycleComponent
+  { initialState: initialState (fromMaybe defaultConfig cfg)
+  , render
   , eval
+  , receiver: const Nothing
   , initializer: Just (H.action Init)
   , finalizer: Nothing
   }
+
   where
   render :: State -> H.ComponentHTML Query
-  render = const $ HH.div [ HP.ref (\elm -> H.action (SetElement elm)) ] []
+  render = const $ HH.div [ HP.ref (H.RefLabel "qrcode") ] []
 
-  eval :: Query ~> H.ComponentDSL State Query m
-  eval (SetElement elm next) = H.modify (_ { element = elm}) $> next
+  eval :: Query ~> H.ComponentDSL State Query Void m
   eval (Init next) = do
-    elm <- H.gets _.element
+    elm <- H.getHTMLElementRef (H.RefLabel "qrcode")
+    H.modify _ { element = elm }
     config <- H.gets _.config
     case elm of
       Nothing -> pure unit
       Just el' -> do
-        qrcode <- H.fromEff $ mkQRCodeNode el' config
+        qrcode <- H.liftEff $ mkQRCodeNode el' config
         H.modify (_ { qrcode = Just qrcode })
         pure unit
     pure next
   eval (SetText content next) = do
-    H.modify (\state -> state { config = state.config { text = content }})
+    H.modify (\state -> state { config = state.config { text = fromMaybe "" content }})
     qrcode <- H.gets _.qrcode
-    maybe (pure unit) (void) $ H.fromEff <<< makeCode content <$> qrcode
+    traverse_ H.liftEff $ makeCode <$> content <*> qrcode
     pure next
